bae92122849069f5d1e5e9dac7c221cce9b662f0
B <107679380+B-00@users.noreply.github.com>
2026-06-22 21:57:06 -0400

    removed unused line

--- app/src/androidTest/java/com/example/androidapplications/TestToolBarTest.java
+++ app/src/androidTest/java/com/example/androidapplications/TestToolBarTest.java
@@ -1,7 +1,7 @@
 package com.example.androidapplications;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
